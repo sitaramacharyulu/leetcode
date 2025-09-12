02cb598a15e16d41ae5db5d8a90f52eb13f188de
vishnupriya <147472761+sitaramacharyulu@users.noreply.github.com>
2025-09-12 06:48:30 +0530

    Time: 6 ms (63.25%) | Memory: 19 MB (79.48%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.cpp
@@ -5,12 +5,10 @@ public:
         int cnt = 0;
         for(int i=0;i<n;i++){
             if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
-                cnt++;
+               return true;
+               break;
             }
         }
-            if(cnt>0){
-                return true;
-            }
         return false;
     }
 };
